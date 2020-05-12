14e1b2687bae5a3f3f9bfd57bb253dff8b8c226b
seolzero <sulyoungpk@gmail.com>
2020-05-12 18:45:33 +0900

    Create Customer.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,51 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Customer from './components/Customer';
+
+const customers = [
+  {
+    'id':'1',
+    'image':'https://placeimg.com/64/64/1',
+    'name': 'Park Seolyeong',
+    'birthday':'941005',
+    'gender':'female',
+    'job':'developer'
+  },
+  {
+    'id':'2',
+    'image':'https://placeimg.com/64/64/2',
+    'name': 'Lee Sangheon',
+    'birthday':'940131',
+    'gender':'male',
+    'job':'developer'
+  },
+  {
+  'id':'3',
+  'image':'https://placeimg.com/64/64/3',
+  'name': 'Siru',
+  'birthday':'190406',
+  'gender':'male',
+  'job':'CUTE'
+  }
+]
 
 function App() {
   return (
-    <div className="gray-background">
-      <img src={logo} lat="logo" />
-      <h2>Let's develop management system!</h2>
-
+    <div>
+      {
+        customers.map(c => {
+          return (
+            <Customer
+              key={c.id}
+              id={c.id}
+              image={c.image}
+              name={c.name}
+              gender={c.gender}
+              job={c.job}
+            />
+          );
+        })
+      }
     </div>
   );
 }
